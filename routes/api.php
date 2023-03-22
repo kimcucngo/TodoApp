f2d5f17ccb42bfae889232928d54fe5ebab6800c
Kim Cuc Ngo <kimcucngo@Kims-MacBook-Air.local>
2023-03-22 12:07:47 +0700

    Task一覧

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::apiResource('/tasks',TaskController::class);
-Route::get('/tasks', [TaskController::class, 'index']);
+// Route::get('/tasks', [TaskController::class, 'index']);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
